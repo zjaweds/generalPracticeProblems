cd6e540b39a99ab92552472d1a92b80970401a6e
Jawed Alam <zjaweds@outlook.com>
2020-10-22 17:52:33 +0530

    Pattern with errors

---
+++ generalProblems/sqrWthDiaAlngXs.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hell's this World!\n");
+
+    int i,j,n;
+    printf("Enter no of lines for the square: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(i>n/2){
+                if(j==(n/2+i/2)||j==(i/2))
+                    printf("*");
+                else
+                {
+                    printf(" ");
+                }
+            }
+            else
+            {   
+                if(j==(n/2)-i||j==(n/2)+i)
+                    printf("*");
+                else
+                {
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
